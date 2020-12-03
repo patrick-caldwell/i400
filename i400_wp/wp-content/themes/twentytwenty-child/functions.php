d5d54700935afd269951eef4e08042de1599bb3d
Patrick <patrickcaldwell1998@gmail.com>
2020-12-03 16:20:48 -0500

    plugins

--- i400_wp/wp-content/themes/twentytwenty-child/functions.php
+++ i400_wp/wp-content/themes/twentytwenty-child/functions.php
@@ -4,3 +4,19 @@ function enqueue_parent_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
+
+add_action('widgets_init', 'twentytwenty_child_widgets_init');
+
+function twentytwenty_child_widgets_init()
+{
+    register_sidebar(array(
+        'name' => __('N413 Sidebar', 'twentytwenty-child'),
+        'id' => 'n413sidebar',
+        'description' => 'A WordPress Sidebar',
+        'class' => 'sidebar',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => '</aside>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ));
+}
